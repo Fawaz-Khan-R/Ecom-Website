876b8b9fc527a17b5e379d3f4c8d420d260eea2b
neojarvis <techadmin@iamneo.ai>
2025-09-10 17:32:03 +0000

    Auto-save-commit-9/10/2025--11:02:01-PM

--- dotnetapp/Repositories/IProductRepository.cs
+++ dotnetapp/Repositories/IProductRepository.cs
@@ -1,27 +1,20 @@
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
-using dotnetapp.DbContext;
 using dotnetapp.Models;
 
 namespace dotnetapp.Repositories
 {
+    public interface IProductRepository
+    {
+        Task<Product> GetProductByIdAsync(int id);
+        Task<IEnumerable<Product>> GetAllProductsAsync();
+        Task AddProductAsync(Product product);
+        Task UpdateProductAsync(Product product);
+        Task DeleteProductAsync(int id);
 
-
-public interface IProductRepository 
-{
-    Product GetProductById(int id);
-    IEnumerable<Product> GetAllProducts();
-    void AddProduct(Product product);
-    void UpdateProduct(Product product);
-    void DeleteProduct(int id);
-
-    // Your async methods
-    Task<IEnumerable<Product>> GetBySellerIdAsync(int sellerId);
-    Task<IEnumerable<Product>> GetApprovedProductsAsync();
-    Task<IEnumerable<Product>> GetProductsByStatusAsync(ProductStatus status);
-    Task<IEnumerable<Product>> SearchProductsAsync(string searchTerm, string category);
-}
+        Task<IEnumerable<Product>> GetBySellerIdAsync(int sellerId);
+        Task<IEnumerable<Product>> GetApprovedProductsAsync();
+        Task<IEnumerable<Product>> GetProductsByStatusAsync(ProductStatus status);
+        Task<IEnumerable<Product>> SearchProductsAsync(string searchTerm, string category);
+    }
 }
